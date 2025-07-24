fd7502b051f56325938b6d59da313b5a1f916de1
daramsreya83@gmail.com <your.email@example.com>
2025-07-24 22:10:02 +0530

    full name

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
-function getFormvalue() {
+function getFormvalue(event) {
     //Write your code here
-
+    event.preventDefault(); 
+    const firstName=document.forms["form1"]["fname"].value;
+    const lastName=document.forms["form1"]["lname"].value;
+    
+    alert(`${firstName} ${lastName}`);
 }
